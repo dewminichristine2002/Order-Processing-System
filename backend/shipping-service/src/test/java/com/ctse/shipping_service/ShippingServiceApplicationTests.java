459756526fd3a3b5f90d335c58c89abb70a41c4d
SanujiSilva <sanujisandanima@gmail.com>
2026-05-03 00:31:48 +0530

    fix shipping test error

--- backend/shipping-service/src/test/java/com/ctse/shipping_service/ShippingServiceApplicationTests.java
+++ backend/shipping-service/src/test/java/com/ctse/shipping_service/ShippingServiceApplicationTests.java
@@ -2,10 +2,19 @@ package com.ctse.shipping_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.ctse.shipping.ShippingServiceApplication;
 
-@SpringBootTest(classes = ShippingServiceApplication.class)
+@SpringBootTest(
+		classes = ShippingServiceApplication.class,
+		properties = {
+				"debug=false",
+				"logging.level.root=INFO",
+				"logging.level.org.springframework=INFO",
+				"spring.jpa.open-in-view=false"
+		})
+@ActiveProfiles("test")
 class ShippingServiceApplicationTests {
 
 	@Test
